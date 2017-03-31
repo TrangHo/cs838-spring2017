d107e467deb1e2e9c70367049c0afaa47dcf8979
TrangHo <carepjan@KuroiTamago.local>
2017-03-31 00:30:23 -0500

    Move clean methods to Cleaner class + use Cleaner in Stage_3.ipynb

---
+++ stage3/src/constants/domains.py
@@ -0,0 +1,17 @@
+COUNTRY = ["ac","ad","ae","af","ag","ai","al","am","an","ap","aq","ar","as","at","au",
+  "az","ba","bb","be","bf","bg","bh","bi","bm","bn","bo","br","bt","by","bz",
+  "ca","cc","cd","cf","cg","ch","ck","cl","cm","cn","co","cr","cu","cx","cy",
+  "cz","de","dj","dk","do","dz","ec","ee","eg","es","fi","fj","fk","fm","fr",
+  "fo","gb","ge","gf","gg","gh","gi","gl","gm","gn","gr","gs","gt","gu","hk",
+  "hm","hn","hr","hu","id","ie","il","im","in","int","io","ir","is","it","je",
+  "jo","jp","ke","kg","kh","kr","kw","ky","kz","lb","lc","li","lk","lr","lt",
+  "lu","lv","ly","mc","md","mg","mh","mk","mm","mn","mo","mp","mq","mr","ms",
+  "mt","mu","mx","my","mw","na","nc","nf","ni","nl","no","np","nu","nz","om",
+  "pa","pe","pg","ph","pk","qa","re","ro","ru","rw","sa","sb","se","sg","sh",
+  "si","sk","sm","sn","so","st","su","sv","sz","tc","td","tf","th","tj","tm",
+  "tn","to","tp","tr","tt","tv","tw","tz","ua","ug","uk","um","us","uy","uz",
+  "ve","vg","vi","vu","wf","ws","yt","yu","za","zm"]
+
+ACADEMIC = ["edu", "ac"]
+
+COMMON = ["net","com","info","org"]
